361f35102877df6ec7587d8af2a72439476ee3f3
lucas <lucas@asia.edu.tw>
2017-09-06 14:52:13 +0800

    aa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("java_0906_1");
         int n;
-        Scanner sc=new Scanner(System.in);
+       Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         int j,sum=0;
         for(j=0;j<=n;j++)
