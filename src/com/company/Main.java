9fbdaefb11bc7d79dba4a9cec65c99e468e79616
lucas <lucas@asia.edu.tw>
2017-09-06 14:48:46 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("java_0906_1");
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int j,sum=0;
+        for(j=0;j<=n;j++)
+            sum+=j;
+        System.out.println("sum="+sum);
+
+    }
+}
